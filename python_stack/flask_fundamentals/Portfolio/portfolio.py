42b05e3d5d109f709079599c1d67c9e2563b8766
Duane Osburn <dosburn@rocketmail.com>
2018-04-05 08:35:37 -0700

    Starting Flask basics

---
+++ python_stack/flask_fundamentals/Portfolio/portfolio.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def portfolio():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def aboutMe():
+    return render_template('about.html')
+
+app.run(debug=True)
