fecd61bb5e4bfe393b4e3ad90ba368ca8a9dc6cf
Duane Osburn <dosburn@rocketmail.com>
2018-04-11 13:15:55 -0700

    Setting up Django

---
+++ python_stack/myEnvironments/djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!i:\coding~1\dojoas~1\python\python~3\myenvi~1\django~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
