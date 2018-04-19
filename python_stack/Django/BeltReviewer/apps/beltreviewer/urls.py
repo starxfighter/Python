c54843845e22e7ab1209d3fb449b5a7d64e851f4
Duane Osburn <dosburn@rocketmail.com>
2018-04-19 10:50:38 -0700

    Added some notes

--- python_stack/Django/BeltReviewer/apps/beltreviewer/urls.py
+++ python_stack/Django/BeltReviewer/apps/beltreviewer/urls.py
@@ -2,15 +2,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),    #done
-    url(r'^register_account/$', views.register_account), #done
-    url(r'^logout_user/$', views.logout),       #done
-    url(r'^login2account/$', views.login),      #done
-    url(r'^add/$', views.add),      #done
+    # login and registration routes
+    url(r'^$', views.index),    
+    url(r'^register_account/$', views.register_account), 
+    url(r'^logout_user/$', views.logout),       
+    url(r'^login2account/$', views.login),  
+    # add and show routes    
+    url(r'^add/$', views.add),      
     url(r'^addBook$', views.book_add),
     url(r'^addReview$', views.review_add),
-    url(r'^book/(?P<book_id>\d+)$', views.show_book), #done
+    url(r'^home/$', views.home),
+    # specific interaction routes
+    url(r'^book/(?P<book_id>\d+)$', views.show_book), 
     url(r'^users/(?P<user_id>\d+)$', views.user_show),    
     url(r'^destroy/(?P<review_id>\d+)$', views.destroy),
-    url(r'^home/$', views.home),
+    
 ]
